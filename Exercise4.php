a2e3577bfb2cd5494dfd6a047a914bad5b4e2b6b
jani72 <deejayjuergen@live.at>
2020-07-13 11:50:44 +0200

    Exercise4

---
+++ Exercise4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+<meta charset="utf-8" />
+    <title>PHP</title>
+</head>
+<body>
+
+<?php
+
+for ($i = 1; $i <= 50; $i++)
+{
+    echo $i . ". Hans-Jürgen Janos <br>";
+}
+
+?>
+
+</body>
+</html>
