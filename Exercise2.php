fd381bb6866b285258455bfee8f4d29e12e508d3
jani72 <deejayjuergen@live.at>
2020-07-13 11:17:14 +0200

    Exercise 2 and 3

---
+++ Exercise2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+<meta charset="utf-8" />
+    <title>PHP</title>
+</head>
+<body>
+
+<?php
+
+$d=date("D");
+if ($d=="Mon" )
+echo "Happy Monday!";
+else
+echo "Have a nice day!" ;
+
+?>
+
+</body>
+</html>
