98e5dd2349e57baed821110b0adc23df2b2c5c7b
jani72 <deejayjuergen@live.at>
2020-07-13 10:48:02 +0200

    Exercise1

---
+++ Exercise1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+<meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css">
+    <title>PHP</title>
+</head>
+<body>
+
+<?php
+
+echo "<h1>Hans-Jürgen Janos</h1>";
+
+?>
+
+</body>
+</html>
